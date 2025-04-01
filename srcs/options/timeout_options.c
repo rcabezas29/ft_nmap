1812441605b782ccb8224d60f6865985436f2667
Raúl Cabezas González <raul.29cg@gmail.com>
2025-04-01 15:40:47 +0200

    Protected timeout value

--- srcs/options/timeout_options.c
+++ srcs/options/timeout_options.c
@@ -2,7 +2,16 @@
 
 int	parse_timeout(const char *timeout)
 {
-	int	timeout_int = ft_atoi(timeout);
+	int timeout_int;
+	for (int i = 0; timeout[i] != '\0'; i++)
+	{
+		if (timeout[i] < '0' || timeout[i] > '9')
+		{
+			ft_putstr_fd("Invalid timeout value\n", 2);
+			exit(EXIT_FAILURE);
+		}
+	}
+	timeout_int = ft_atoi(timeout);
 	if (timeout_int < 0)
 	{
 		ft_putstr_fd("Invalid timeout value\n", 2);
